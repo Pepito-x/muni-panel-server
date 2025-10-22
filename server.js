6bb3ce6671e14709851f7c71e3b5228fbf2c062c
Anderson Giron <gironvillegasandersonbenjamin@gmail.com>
2025-10-22 02:04:11 -0500

    Cambiar remitente a Gmail verificado

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import sgMail from "@sendgrid/mail";
+import sgMail from "minitec.sailuker@gmail.com";
 
 const app = express();
 app.use(cors());
